46853ad6e4f91b0f44df74262139a2ad748c2360
Sherief Farid <sherief@farid.cloud>
2019-03-13 03:45:51 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,10 +1,10 @@
-passengers = {
-suite_a: "Amanda Presley",
-suite_b: "Seymour Hoffman",
-suite_c: "Alfred Tennyson",
-suite_d: "Charlie Chaplin",
-suite_e: "Crumpet the Elf"
-}
+# passengers = {
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
+# suite_e: "Crumpet the Elf"
+# }
 
 def select_winner(passengers)
   # add the code snippet here!
@@ -13,8 +13,9 @@ def select_winner(passengers)
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
-    #return winner
   end
+
+  winner
 end
 
 
